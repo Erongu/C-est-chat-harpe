3656ff827803b7a5c8ba61e7f6ca38a7ada06060
BornToWard <julien.his@viacesi.fr>
2018-12-06 15:54:28 +0100

    Four.cs created.

--- Projet/Projet/Model/Four.cs
+++ Projet/Projet/Model/Four.cs
@@ -8,5 +8,13 @@ namespace Projet.Model
 {
     class Four
     {
+       private List<Ingredient> ingredients;
+       private bool etat; //Definit si le four est en cours d utilsation
+
+        public void cuisson(int temps, List<Ingredient> ingredients)
+        {
+            Thread.Sleep(1000*temps);
+            return ingredients;
+        }
     }
 }
