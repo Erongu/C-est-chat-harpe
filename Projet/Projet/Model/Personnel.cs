cd03c84321fc63d972ddb5e7fca4af6e48226f20
bjorn <romain.jouatte@viacesi.fr>
2018-12-06 16:14:07 +0100

    ajout de la classe personnel

--- Projet/Projet/Model/Personnel.cs
+++ Projet/Projet/Model/Personnel.cs
@@ -2,6 +2,56 @@ namespace Model
 {
     public class Personnel
     {
+        private int ID;
+        private string prenom;
+        private string nom;
+        private string metier;
+        private int posX;
+        private int posY;
 
+
+       
+
+        //Getter 
+        public int getID()
+        {
+            return this.ID;
+        }
+
+        public string getPrenom()
+        {
+            return this.prenom;
+        }
+
+        public string getNom()
+        {
+            return this.nom;
+        }
+
+        public string getMetier()
+        {
+            return this.metier;
+        }
+
+        public int getPosX()
+        {
+            return this.posX;
+        }
+        public int getPosY()
+
+        {
+            return this.posY;
+        }
+
+        public void setPosX(int posX)
+        {
+            this.posX = posX;
+        }
+
+        public void setPosY(int posY)
+        {
+            this.posY = posY;
+        }
     }
 }
+
