fbd77534dda199d1c7a80606b8508e777d9441ed
Nenrikido <ampy00@live.fr>
2018-12-06 11:57:45 +0100

    Change le controlleur

--- Projet/Projet/Controller/RestaurantController.cs
+++ Projet/Projet/Controller/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,6 +12,12 @@ namespace Controller
         static void Main(string[] args)
         {
             Console.WriteLine("IZII");
+            Factory factory = new Factory("Serveur"); 
+
+            serveur = factory.CreateObject();
+
+            Console.WriteLine(serveur.GetType());
+            Console.WriteLine("IZI");
             Console.ReadKey(true);
         }
     }
