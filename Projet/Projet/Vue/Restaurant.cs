b50356a270dc5b2a00af9800bd14fe57daebf959
Chococraco <etienne.jouvencon@wanadoo.fr>
2018-12-07 12:32:23 +0100

    Modification vue

--- Projet/Projet/Vue/Restaurant.cs
+++ Projet/Projet/Vue/Restaurant.cs
@@ -1,7 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
 namespace View
 {
-    public class Restaurant
+    public class Restaurant : Form
     {
+        private Panel font;
 
+        //Constructeur qui affiche la map
+        public Restaurant()
+        {
+            this.Height = 900;
+            this.Width = 1400;
+
+            //Création du fond
+            font = new Panel();
+            font.SetBounds(1,1, 1400, 900);
+            font.BackgroundImage = Projet.Properties.Resources.cuisine;
+            this.Controls.Add(font);//Ajout du font au form
+        }
     }
 }
