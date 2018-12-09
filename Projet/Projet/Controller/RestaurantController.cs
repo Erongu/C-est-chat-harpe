dc557e4d4a410c1122cf3f3904d1f11e688940fb
Erongu <dmeschin@msn.com>
2018-12-09 14:51:13 +0100

    Update RestaurantController.cs

--- Projet/Projet/Controller/RestaurantController.cs
+++ Projet/Projet/Controller/RestaurantController.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿using Projet.Controller.Network.Server;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 using View;
 
 namespace Controller
@@ -16,23 +19,36 @@ namespace Controller
         static void Main(string[] args)
         {
             vue = new View.Restaurant();
-            vue.ShowDialog();
-            Console.ReadKey(true);
+            vue.Show();
 
             Personnel serveur = new Factory().Create<Personnel>();
-            
+
+            NetworkController.Instance.Start("127.0.0.1", 8500);
+
+            var client = new Projet.Controller.Network.Client.Client();
+            client.Connect("127.0.0.1", 8500);
+
             Console.WriteLine("====");
+
             string[] MethodNames = serveur.GetType().GetMethods().Select(I => I.Name).ToArray();
+
             foreach (string method in MethodNames)
             {
                 Console.WriteLine(method);
             }
+
             serveur.method("Serve");
             Console.WriteLine("====");
             Console.WriteLine(serveur.GetType());
-            
 
-            
+
+            while (true)
+            {
+                Thread.Sleep(10);
+                Application.DoEvents();
+            }
+
+
         }
     }
 }
