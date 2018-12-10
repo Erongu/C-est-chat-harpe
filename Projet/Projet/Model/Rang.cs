eeefa07831cc93a1833e2fc8d2d03355de1e6a2a
BornToWard <julien.his@viacesi.fr>
2018-12-10 15:22:24 +0100

    Fixed Rang.cs, because of Table.cs changes. X and Y still have to be defined in Rang.cs

--- Projet/Projet/Model/Rang.cs
+++ Projet/Projet/Model/Rang.cs
@@ -28,47 +28,47 @@ namespace Model
             //Création des tables
             if((numeroCarre == 1)&&(numeroRang == 1))
             {
-                Tables.Add(new Table(1, 4));
-                Tables.Add(new Table(2, 4));
-                Tables.Add(new Table(3, 4));
-                Tables.Add(new Table(4, 4));
-                Tables.Add(new Table(5, 4));
-                Tables.Add(new Table(6, 2));
-                Tables.Add(new Table(7, 2));
-                Tables.Add(new Table(8, 2));
+                Tables.Add(new Table(1, 4, 1, 1, 0, 0));
+                Tables.Add(new Table(2, 4, 1, 1, 0, 0));
+                Tables.Add(new Table(3, 4, 1, 1, 0, 0));
+                Tables.Add(new Table(4, 4, 1, 1, 0, 0));
+                Tables.Add(new Table(5, 4, 1, 1, 0, 0));
+                Tables.Add(new Table(6, 2, 1, 1, 0, 0));
+                Tables.Add(new Table(7, 2, 1, 1, 0, 0));
+                Tables.Add(new Table(8, 2, 1, 1, 0, 0));
             }
             if ((numeroCarre == 1) && (numeroRang == 2))
             {
-                Tables.Add(new Table(9, 2));
-                Tables.Add(new Table(10, 2));
-                Tables.Add(new Table(11, 6));
-                Tables.Add(new Table(12, 6));
-                Tables.Add(new Table(13, 6));
-                Tables.Add(new Table(14, 8));
-                Tables.Add(new Table(15, 8));
-                Tables.Add(new Table(16, 10));
+                Tables.Add(new Table(9, 2, 2, 1, 0, 0));
+                Tables.Add(new Table(10, 2, 2, 1, 0, 0));
+                Tables.Add(new Table(11, 6, 2, 1, 0, 0));
+                Tables.Add(new Table(12, 6, 2, 1, 0, 0));
+                Tables.Add(new Table(13, 6, 2, 1, 0, 0));
+                Tables.Add(new Table(14, 8, 2, 1, 0, 0));
+                Tables.Add(new Table(15, 8, 2, 1, 0, 0));
+                Tables.Add(new Table(16, 10, 2, 1, 0, 0));
             }
             if ((numeroCarre == 2) && (numeroRang == 1))
             {
-                Tables.Add(new Table(17, 4));
-                Tables.Add(new Table(18, 4));
-                Tables.Add(new Table(19, 4));
-                Tables.Add(new Table(20, 4));
-                Tables.Add(new Table(21, 4));
-                Tables.Add(new Table(22, 2));
-                Tables.Add(new Table(23, 2));
-                Tables.Add(new Table(24, 2));
+                Tables.Add(new Table(17, 4, 1, 2, 0, 0));
+                Tables.Add(new Table(18, 4, 1, 2, 0, 0));
+                Tables.Add(new Table(19, 4, 1, 2, 0, 0));
+                Tables.Add(new Table(20, 4, 1, 2, 0, 0));
+                Tables.Add(new Table(21, 4, 1, 2, 0, 0));
+                Tables.Add(new Table(22, 2, 1, 2, 0, 0));
+                Tables.Add(new Table(23, 2, 1, 2, 0, 0));
+                Tables.Add(new Table(24, 2, 1, 2, 0, 0));
             }
             if ((numeroCarre == 2) && (numeroRang == 2))
             {
-                Tables.Add(new Table(25, 2));
-                Tables.Add(new Table(26, 2));
-                Tables.Add(new Table(27, 6));
-                Tables.Add(new Table(28, 6));
-                Tables.Add(new Table(29, 8));
-                Tables.Add(new Table(30, 8));
-                Tables.Add(new Table(31, 8));
-                Tables.Add(new Table(32, 10));
+                Tables.Add(new Table(25, 2, 2, 2, 0, 0));
+                Tables.Add(new Table(26, 2, 2, 2, 0, 0));
+                Tables.Add(new Table(27, 6, 2, 2, 0, 0));
+                Tables.Add(new Table(28, 6, 2, 2, 0, 0));
+                Tables.Add(new Table(29, 8, 2, 2, 0, 0));
+                Tables.Add(new Table(30, 8, 2, 2, 0, 0));
+                Tables.Add(new Table(31, 8, 2, 2, 0, 0));
+                Tables.Add(new Table(32, 10, 2, 2, 0, 0));
             }
         }
 
