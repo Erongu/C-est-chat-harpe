4081059ed57dcde06b8c8fd667482fc35cc49a74
BornToWard <julien.his@viacesi.fr>
2018-12-12 09:16:07 +0100

    StrategyCommis

---
+++ Projet/Projet/Controller/Strategy/Salle/StrategyCommis.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projet.Controller.Strategy.Cuisine
+{
+    public class StrategyCommis
+    {
+
+
+        public void MettrePain(int numeroTable)
+        {
+
+        }
+
+        public void MettreBouteille(int numeroTable)
+        {
+
+        }
+    }
+}
