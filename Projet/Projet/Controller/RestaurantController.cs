3aa9e98ab93661f63f324659503e6626b1892ac3
Nenrikido <ampy00@live.fr>
2018-12-14 09:53:48 +0100

    Change all runs

--- Projet/Projet/Controller/RestaurantController.cs
+++ Projet/Projet/Controller/RestaurantController.cs
@@ -303,7 +303,8 @@ namespace Controller
 
                 foreach (var plongeur in plongeurs)
                 {
-
+                    object ustensiles = restaurant.Evier.Ustensiles();
+                    plongeur.Call("Plonge", ustensiles);
                 }
 
             }
@@ -311,22 +312,25 @@ namespace Controller
 
         static private void RunLaveVaisselle()
         {
-
+            Model.Cuisine.LaveVaisselle laveVaisselle = new Model.Cuisine.LaveVaisselle();
+            laveVaisselle.VideLaveVaisselle();
         }
 
         static private void RunMachineLaver()
         {
-
+            Model.Cuisine.MachineLaver machineLaver = new Model.Cuisine.MachineLaver();
+            machineLaver.VideMachineLaver();
         }
 
         static private void RunFour()
         {
-
+            Model.Cuisine.Four four = new Model.Cuisine.Four();
+            four.cuisson();
         }
 
         static private void RunPlaque()
         {
-
+            Model.Cuisine.Plaque plaque = new Model.Cuisine.Plaque();
         }
 
         private static void SpawnNpc()
